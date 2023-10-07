39ea2fe2f18ba83be38ccd5d75bdd4b3b5cb0988
Elizabeth McDonald <elizabethmcd4@gmail.com>
2023-10-07 16:26:44 -0700

    removed old createSalmon function comment

--- src/world_init.cpp
+++ src/world_init.cpp
@@ -37,33 +37,6 @@ Entity createTestSalmon(RenderSystem* renderer, vec2 pos)
 }
 
 /*
-Entity createSalmon(RenderSystem* renderer, vec2 pos)
-{
-	auto entity = Entity();
-
-	// Store a reference to the potentially re-used mesh object
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::SALMON);
-	registry.meshPtrs.emplace(entity, &mesh);
-
-	// Setting initial motion values
-	Motion& motion = registry.motions.emplace(entity);
-	motion.position = pos;
-	motion.angle = 0.f;
-	motion.velocity = { 0.f, 0.f };
-	motion.scale = mesh.original_size * 150.f;
-	motion.scale.x *= -1; // point front to the right
-
-	// Create and (empty) Salmon component to be able to refer to all turtles
-	registry.players.emplace(entity);
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::TEXTURE_COUNT, // TEXTURE_COUNT indicates that no txture is needed
-			EFFECT_ASSET_ID::SALMON,
-			GEOMETRY_BUFFER_ID::SALMON });
-
-	return entity;
-}
-
 Entity createFish(RenderSystem* renderer, vec2 position)
 {
 	// Reserve en entity
