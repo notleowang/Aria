0a95e16f3c30e27751dfbb8b191eb4c22c8b956c
notleowang <notleo@students.cs.ubc.ca>
2023-10-28 19:30:04 -0700

    Edited level size

--- src/game_level.cpp
+++ src/game_level.cpp
@@ -1,5 +1,8 @@
 #include "game_level.hpp"
 
+#include <ft2build.h>
+#include FT_FREETYPE_H  
+
 bool GameLevel::init(uint level) {
 	printf("Initializing game for level: %i\n", level);
 	if (level < 0) {
@@ -26,35 +29,30 @@ bool GameLevel::init(uint level) {
 
 		FT_Set_Pixel_Sizes(face, 0, 48);
 
-
-
 		floors.clear();
-		for (uint i = 0; i < 6; i++) {
-			for (uint j = 0; j < 4; j++) {
-				floors.push_back(vec2(225 + i * 250, 175 + j * 250));
+		for (uint i = 0; i < 5; i++) {
+			for (uint j = 0; j < 3; j++) {
+				floors.push_back(vec2(200 + i * 250, 300 + j * 250));
 			}
 		}
-		for (uint i = 0; i < 4; i++) {
-			floors.push_back(vec2(1600, 175 + i * 250));
-		}
 
-		this->player_starting_pos = vec2(200, 700);
+		this->player_starting_pos = vec2(500, 500);
 		this->exit_door_pos = vec2(1500, 950);
 
 		terrains.clear();
 
-		terrains.push_back(vec4(900, 0, 1800, 100));
-		terrains.push_back(vec4(50, 500, 100, 900));
-		terrains.push_back(vec4(900, 1000, 1800, 100));
-		terrains.push_back(vec4(1750, 500, 100, 900));
-		terrains.push_back(vec4(400, 700, 100, 600));
-		terrains.push_back(vec4(800, 300, 100, 600));
-		terrains.push_back(vec4(1200, 700, 100, 600));
+		terrains.push_back(vec4(675, 200, 1400, 100));
+		terrains.push_back(vec4(25, 550, 100, 800));
+		terrains.push_back(vec4(675, 900, 1400, 100));
+		terrains.push_back(vec4(1325, 550, 100, 800));
+		//terrains.push_back(vec4(400, 700, 100, 600));
+		//terrains.push_back(vec4(800, 300, 100, 600));
+		//terrains.push_back(vec4(1200, 700, 100, 600));
 
 		enemies.clear();
 
-		enemies.push_back({ 800, 700, 100, 100, 0, 0 });
-		enemies.push_back({ 1200, 300, 100, 100, 0, 0 });
+		//enemies.push_back({ 800, 700, 100, 100, 0, 0 });
+		//enemies.push_back({ 1200, 300, 100, 100, 0, 0 });
 		break;
 	case 1:
 		floors.clear();
