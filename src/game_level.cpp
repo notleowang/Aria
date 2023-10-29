101fdc9d032b15ca7a051e60045a4e777c17dba4
ngynjsn <ngynjsn@students.cs.ubc.ca>
2023-10-29 15:03:18 -0700

    can move between levels

--- src/game_level.cpp
+++ src/game_level.cpp
@@ -5,9 +5,13 @@ bool GameLevel::init(uint level) {
 	if (level <= 0) {	
 		return false;
 	}
+	this->curr_level = level;
 	std::vector<vec2>& floors = this->floor_pos;
 	std::vector<vec4>& terrains = this->terrains_attr;
 	std::vector<std::array<float, ENEMY_ATTRIBUTES>>& enemies = this->enemies_attr;
+	floors.clear();
+	terrains.clear();
+	enemies.clear();
 
 	switch (level) {
 		case 1:
@@ -38,7 +42,17 @@ bool GameLevel::init(uint level) {
 			break;
 		case 2:
 			// Temporary level 2
+			for (uint i = 0; i < 6; i++) {
+				for (uint j = 0; j < 4; j++) {
+					floors.push_back(vec2(225 + i * 250, 175 + j * 250));
+				}
+			}
+			for (uint i = 0; i < 4; i++) {
+				floors.push_back(vec2(1600, 175 + i * 250));
+			}
+
 			this->player_starting_pos = vec2(400, 400);
+			this->exit_door_pos = vec2(1500, 950);
 			terrains.push_back(vec4(600, 400, 100, 100));
 			terrains.push_back(vec4(600, 50, 1000, 100));
 			terrains.push_back(vec4(50, 400, 100, 800));
