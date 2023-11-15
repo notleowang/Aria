371f6ab7c330cb2de469ea1552e557b2a559c5bd
Elizabeth McDonald <emcd4@students.cs.ubc.ca>
2023-11-14 20:44:45 -0800

    added blur to edges of spotlight

--- shaders/screen_darken.fs.glsl
+++ shaders/screen_darken.fs.glsl
@@ -17,21 +17,31 @@ vec4 fade_color(vec4 in_color)
 	return in_color;
 }
 
-void main()
+vec4 spotlight(vec4 in_color)
 {
-	vec4 in_color = texture(screen_texture, texcoord);
-	color = in_color;
-
 	vec2 coord_px = texcoord * window_size;
 	float dist_px = distance(vec2(window_size.x/2.0, window_size.y/2.0), coord_px);
 	float scale_factor = max(window_size.x, window_size.y) * 0.75;
 	float radius_px = radius * scale_factor;
 
-	if (apply_spotlight && dist_px > radius_px) {
-		color.r = 0.0;
-		color.g = 0.0;
-		color.b = 0.0;
-	}
+	float blurDistance = 25.0;
+    float diff = radius_px - blurDistance;
 
+	if (dist_px < radius_px) {
+		if (dist_px > diff) {
+			float diff2 = radius_px - dist_px;
+			return in_color * (diff2/blurDistance);
+		} else {
+			return in_color;
+		}
+	} else {
+		return vec4(0.0, 0.0, 0.0, 1.0);
+	}
+}
+
+void main()
+{
+	vec4 in_color = texture(screen_texture, texcoord);
+	color = apply_spotlight ? spotlight(in_color) : in_color;
 	color = fade_color(color);
 }
