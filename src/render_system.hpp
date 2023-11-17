52de5efc40f62a2f9e6fba080890114d44ba6bb1
ngynjsn <ngynjsn@students.cs.ubc.ca>
2023-11-17 00:12:28 -0800

    buggy on computer recommit

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -133,7 +133,6 @@ private:
 	void initializeExitDoorGeometryBuffer();
 	void initializeResourceBarGeometryBuffer();
 	void initializeSpriteSheetGeometryBuffer(GEOMETRY_BUFFER_ID goem_buffer_id, int num_rows, int num_cols);
-	void initializeShadowMap();
 
 	// Window handle
 	GLFWwindow* window;
